a9feaaf378e89fb146817bd45d1efcacd4b715d0
Tollro <2821632823@qq.com>
2025-03-11 09:13:13 +0800

    download_tensorflow_check1

---
+++ main.py
@@ -0,0 +1,32 @@
+import tensorflow as tf
+from tensorflow.keras.datasets import mnist
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Flatten
+from tensorflow.keras.utils import to_categorical
+
+# 加载MNIST数据集
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+# 预处理数据
+x_train = x_train.astype('float32') / 255
+x_test = x_test.astype('float32') / 255
+y_train = to_categorical(y_train, 10)
+y_test = to_categorical(y_test, 10)
+
+# 构建模型
+model = Sequential([
+    Flatten(input_shape=(28, 28)),
+    Dense(128, activation='relu'),
+    Dense(64, activation='relu'),
+    Dense(10, activation='softmax')
+])
+
+# 编译模型
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+# 训练模型
+model.fit(x_train, y_train, epochs=10, batch_size=32, validation_data=(x_test, y_test))
+
+# 评估模型
+test_loss, test_acc = model.evaluate(x_test, y_test)
+print(f'Test accuracy: {test_acc:.2f}')
